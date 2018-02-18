f8e5d950abc49a7635a5049d2d5bced4adb983d2
ttensfeldt <36460785+ttensfeldt@users.noreply.github.com>
2018-02-18 15:24:06 -0500

    Add files via upload

---
+++ Main4.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+        public class Main4 {
+
+            public static void main(String[] args) {
+
+                Scanner scan = new Scanner(System.in);
+
+                System.out.print("Enter a hexadecimal number: ");
+
+                String input = scan.nextLine();
+
+                if(input.charAt(0) == '0' && input.charAt(1) == 'x'){   //remove 0x
+
+                    input = input.substring(2);
+
+                }
+
+                int len = input.length();
+                input = input.toUpperCase();    //make it all upper
+
+                long out = 0;
+
+                for(int i = 0; i < len; i++){   //iterating through all characters in the string
+
+                    if(input.charAt(i) >= 65 && input.charAt(i) <= 90){ //check if its a letter
+
+                        out += Math.pow(16,(len-(i+1))) * (input.charAt(i)-55);
+
+
+                    }
+                    else{
+
+                        out += Math.pow(16,(len-(i+1))) * (input.charAt(i)-48);
+
+
+                    }
+
+                }
+
+
+                System.out.println( "Your number is " + out + " in decimal");
+
+            }
+        }
+
